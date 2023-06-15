9469167369b564653d9572ed068f9f79350a974a
eliasbelzberg <ebelz@uw.edu>
2023-06-15 15:32:49 -0700

    file clean up

--- app.js
+++ app.js
@@ -1,42 +1,15 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
+// Top K results from featured products page
+const TOPK_PROD = 30;
+const SKI_URL = "/shop/ski/skis";
+const SNB_URL = "/shop/snowboard/snowboards";
+const SCRAPE_URL = 'https://www.evo.com' + SNB_URL + "/rpp_" + TOPK_PROD;
+
 const main = async () => {
-  convertSkiJsonToCsv("data/skiData200.json", "data/skiData200.csv");
+  await scrape();
 };
-
-function convertSkiJsonToCsv(jsonFilePath, outputPath) {
-  const jsonData = JSON.parse(fs.readFileSync(jsonFilePath));
-
-  const csvHeader = 'productType,productName,price,url,description,spec\n';
-  let csvContent = '';
-
-  jsonData.forEach(item => {
-    const {
-      productName,
-      price,
-      url,
-      details: {
-        description,
-        terrain,
-        abilityLevel,
-        rockerType,
-        turningRadius,
-        tailType,
-        materials,
-        warranty
-      }
-    } = item;
-
-    const csvRow = `"Ski", "${productName}","${price}","${url}","${description}","Terrain: ${terrain} | Ability Level: ${abilityLevel} | Rocker Type: ${rockerType} | Turning Radius: ${turningRadius} | Tail Type: ${tailType} | Core/Laminates: ${materials} | Warranty: ${warranty}"\n`;
-    csvContent += csvRow;
-  });
-
-  const csvData = csvHeader + csvContent;
-
-  fs.writeFileSync(outputPath, csvData);
-  console.log('CSV file generated successfully:', outputPath);
-}
 
 async function scrape() {
   let browser;
@@ -47,11 +20,13 @@ async function scrape() {
     const page = await browser.newPage();
     page.setDefaultNavigationTimeout(2 * 60 * 1000);
 
-    await page.goto('https://www.evo.com/shop/ski/skis/rpp_200');
+    console.log("/!\\ Starting scrape /!\\");
+
+    // Modify constant to search snb/ski and number of products scraped
+    await page.goto(SCRAPE_URL);
 
     productData = await page.evaluate(() => {
       const products = Array.from(document.querySelectorAll('.product-thumb'));
-
       return products.map(product => {
         const name = product.querySelector('.product-thumb-title').innerText;
         const priceElement = product.querySelector('.product-thumb-price');
@@ -65,45 +40,44 @@ async function scrape() {
       });
     });
 
+    // Scrapes products subpages
     for (const product of productData) {
+      console.log(`Starting ${product['productName']}...`);
+
+      // Interval between subpage requests
       await new Promise(resolve => setTimeout(resolve, 2500));
       await page.goto(product['url']);
+
+      // !!! Uncomment code to change schema for ski/snb !!!
       product.details = await page.evaluate(() => {
-        //const tableRows = Array.from(document.querySelectorAll('.table-responsive table tbody tr'));
-        //const sizes = [];
-        // for (const row of tableRows) {
-        //   const sizeCell = row.querySelector('th');
-        //   const sizeValue = sizeCell.textContent.trim();
-        //   if (sizeValue.includes('Size (cm)')) {
-          //     const cmCells = Array.from(row.querySelectorAll('td'));
-          //     const cmValues = cmCells.map(cell => cell.textContent.trim());
-          //     sizes.push(cmValues + "cm");
-          //   }
-          // }
-        // FOR SKI
+
+        //// =========FOR SKI=============
+        // let data = {
+        //   description: document.querySelector(".pdp-details-content p")?.textContent?.trim() ?? "",
+        //   terrain: document.querySelector(".spec-terrain .pdp-spec-list-description")?.textContent?.trim() ?? "",
+        //   abilityLevel: document.querySelector(".spec-ability-level .pdp-spec-list-description")?.textContent?.trim() ?? "",
+        //   rockerType: document.querySelector(".spec-rocker-type .pdp-spec-list-description")?.textContent?.trim() ?? "",
+        //   turningRadius: document.querySelector(".spec-turning-radius .pdp-spec-list-description")?.textContent?.trim() ?? "",
+        //   tailType: document.querySelector(".spec-tail-type .pdp-spec-list-description")?.textContent?.trim() ?? "",
+        //   materials: document.querySelector(".spec-core-laminates .pdp-spec-list-description")?.textContent?.trim() ?? "",
+        //   warranty: document.querySelector(".spec-warranty .pdp-spec-list-description")?.textContent?.trim() ?? ""
+        //   // ,
+        //   // sizes: sizes?.toString()?.trim() ?? ""
+        // };
+
+        // ======FOR SNOWBOARD=========
         let data = {
           description: document.querySelector(".pdp-details-content p")?.textContent?.trim() ?? "",
           terrain: document.querySelector(".spec-terrain .pdp-spec-list-description")?.textContent?.trim() ?? "",
           abilityLevel: document.querySelector(".spec-ability-level .pdp-spec-list-description")?.textContent?.trim() ?? "",
           rockerType: document.querySelector(".spec-rocker-type .pdp-spec-list-description")?.textContent?.trim() ?? "",
-          turningRadius: document.querySelector(".spec-turning-radius .pdp-spec-list-description")?.textContent?.trim() ?? "",
-          tailType: document.querySelector(".spec-tail-type .pdp-spec-list-description")?.textContent?.trim() ?? "",
+          shape: document.querySelector(".spec-shape .pdp-spec-list-description")?.textContent?.trim() ?? "",
+          flexRating: document.querySelector(".spec-flex-rating .pdp-spec-list-description")?.textContent?.trim() ?? "",
           materials: document.querySelector(".spec-core-laminates .pdp-spec-list-description")?.textContent?.trim() ?? "",
           warranty: document.querySelector(".spec-warranty .pdp-spec-list-description")?.textContent?.trim() ?? ""
-          // ,
-          // sizes: sizes?.toString()?.trim() ?? ""
-        };
-        // FOR SNOWBOARD
-        // let data = {
-        //   description: document.querySelector(".pdp-details-content p").textContent.trim(),
-        //   terrain: document.querySelector(".spec-terrain .pdp-spec-list-description").textContent.trim(),
-        //   abilityLevel: document.querySelector(".spec-ability-level .pdp-spec-list-description").textContent.trim(),
-        //   rockerType: document.querySelector(".spec-rocker-type .pdp-spec-list-description").textContent.trim(),
-        //   shape: document.querySelector(".spec-shape .pdp-spec-list-description").textContent.trim(),
-        //   flexRating: document.querySelector(".spec-flex-rating .pdp-spec-list-description").textContent.trim(),
-        //   materials: document.querySelector(".spec-core-laminates .pdp-spec-list-description").textContent.trim(),
-        //   warranty: document.querySelector(".spec-warranty .pdp-spec-list-description").textContent.trim()
-        // }
+        }
+
+        console.log("Completed")
         return data;
       });
     };
@@ -114,8 +88,8 @@ async function scrape() {
       await browser.close();
     }
     fs.writeFileSync('data/productData.json', JSON.stringify(productData, null, 2));
+    console.log("/!\\ Done /!\\")
   }
-
 }
 
 main();
